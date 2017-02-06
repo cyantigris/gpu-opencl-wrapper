5d3d78155a64c405fe1e7ffb56a97025eb009a3e
jeff <jeff@swarmgate.com>
2017-02-06 06:07:46 -0800

    add comment

--- example/withWrapper.js
+++ example/withWrapper.js
@@ -25,31 +25,37 @@ let init = function(){
     //allocate GPU memeryBuffer
     for(let i=0;i< inputData.length;i++)
     {
-        cl.enqueueWriteBuffer (clWrapper._commandQueue, bufferList[i], true, 0, inputData[i].length*Uint32Array.BYTES_PER_ELEMENT, inputData[i]);
+        cl.enqueueWriteBuffer(
+            clWrapper._commandQueue,                                //cl command queue
+            bufferList[i],                                          //GPU buffer
+            true,                                                   //blocking or non-blocking
+            0,                                                      //offset
+            inputData[i].length*Uint32Array.BYTES_PER_ELEMENT,      //the size in bytes of data
+            inputData[i]                                            //data itself (because no pointer in js)
+        );
     }
 
     //set C kernel arguments
     clWrapper.setKernelArg(
-        kernel,
-        [
-            {type:'uint*',data:bufferList[0]},
-            {type:'uint*',data:bufferList[1]},
-            {type:'uint*',data:bufferList[2]},
-            {type:'uint',data:BUFFER_SIZE}
+        kernel,[
+            {type:'uint*',data:bufferList[0]},      //arg 1
+            {type:'uint*',data:bufferList[1]},      //arg 2
+            {type:'uint*',data:bufferList[2]},      //arg 3
+            {type:'uint',data:BUFFER_SIZE}          //arg 4
         ]
     );
 
     // run openCL
     cl.enqueueNDRangeKernel(
-        clWrapper._commandQueue, 
-        kernel, 
-        1,
-        null,
-        [BUFFER_SIZE],
-        null
+        clWrapper._commandQueue,        //cl command queue
+        kernel,                         //cl kernel
+        1,                              //work-dimension 1-d , 2-d , 3-d
+        null,                           //global work offset
+        [BUFFER_SIZE],                  //global work size
+        null                            //local work size
     );
 
-    // get results and block while getting them
+    // get results and block while getting them; (input structure see 'cl.enqueueWriteBuffer' above);
     cl.enqueueReadBuffer (clWrapper._commandQueue, bufferList[2], true, 0, inputData[2].length*Uint32Array.BYTES_PER_ELEMENT, inputData[2]);
     
     _printResults(inputData,BUFFER_SIZE);
